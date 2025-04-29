4de204d68f59f561e8a4daa06a8f12de6ba557b3
minxrka <minorka04@mail.ru>
2025-04-30 01:25:49 +0300

    *types *type *events/telegram

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,13 +1,79 @@
 package telegram
 
-import "github.com/yowie645/ReadItLaterBot/clients/telegram"
+import (
+	"github.com/yowie645/ReadItLaterBot/clients/telegram"
+	"github.com/yowie645/ReadItLaterBot/events"
+	"github.com/yowie645/ReadItLaterBot/lib/e"
+	"github.com/yowie645/ReadItLaterBot/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New(client *telegram.Client) {
+type Meta struct {
+	ChatID   int
+	Username string
+}
 
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
+		res = append(res, event(u))
+	}
+
+	p.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+
+	return upd.Message.Text
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+	return events.Message
 }
