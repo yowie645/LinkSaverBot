30599872bf8a112a9989a4366618e168b9915580
minxrka <minorka04@mail.ru>
2025-05-01 19:23:30 +0300

    *sqlite +Save

--- storage/sqlite/sqlite.go
+++ storage/sqlite/sqlite.go
@@ -1,8 +1,11 @@
 package sqlite
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+
+	"github.com/yowie645/ReadItLaterBot/storage"
 )
 
 type Storage struct {
@@ -18,4 +21,13 @@ func New(path string) (*Storage, error) {
 		return nil, fmt.Errorf("can't connect to database: %w", err)
 	}
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) Save(ctx context.Context, p *storage.Page) error {
+	q := `INSERT INFO pages (url, user_name) VALUES (?, ?)`
+
+	if _, err := s.db.ExecContext(ctx, q, p.URL, p.UserName); err != nil {
+		return fmt.Errorf("can't save page: %w", err)
+	}
+	return nil
 }
