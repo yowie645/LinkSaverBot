a8164af1ebadec8d17e5d1d5213e97deeac18f66
minxrka <minorka04@mail.ru>
2025-05-01 21:26:44 +0300

    *sqlite +Remove

--- storage/sqlite/sqlite.go
+++ storage/sqlite/sqlite.go
@@ -50,3 +50,11 @@ func (s *Storage) PickRandom(ctx context.Context, userName string) (*storage.Pag
 		UserName: userName,
 	}, nil
 }
+
+func (s *Storage) Remove(ctx context.Context, page *storage.Page) error {
+	q := `DELETE FROM pages WHERE url = ? AND user_name = ?`
+	if _, err := s.db.ExecContext(ctx, q, page.URL, page.UserName); err != nil {
+		return fmt.Errorf("can't remove page: %w", err)
+	}
+	return nil
+}
